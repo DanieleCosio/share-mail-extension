8a00e18d9540edf621370e45502cc01bb8520249
daniele.cosio <daniele.cosio99@gmail.com>
2024-06-10 01:47:58 +0200

    feat: register components

--- src/components/Components.ts
+++ src/components/Components.ts
@@ -1,4 +1,8 @@
 import globalCss from "./globals.css";
+import "./Btn/Btn";
+import "./Loader/Loader";
+import "./Modal/Modal";
+import "./Icon/Icon";
 
 export class Components {
     static load() {
