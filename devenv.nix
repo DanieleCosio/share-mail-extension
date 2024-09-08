5b1e51bd9a755005a8b42e25abd57befeb769e2b
daniele.cosio <daniele.cosio99@gmail.com>
2024-09-08 19:22:46 +0200

    refactor: monorepo setup

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,35 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+  languages.go.enable = true;
+
   languages.javascript.enable = true;
   languages.javascript.package = pkgs.nodejs-18_x;
+
+  packages = [ pkgs.coreutils pkgs.go-migrate pkgs.sqlc ];
+
+  services.postgres = {
+    enable = true;
+    initialDatabases = [{ name = "share_mail"; }];
+  };
+
+  pre-commit.hooks = {
+    gofmt.enable = true;
+    govet.enable = true;
+    gotest.enable = true;
+    commitizen.enable = true;
+  };
+
+  scripts = {
+    db-migrate.exec = ''
+      migrate -path apps/backend/database/migrations -database "postgresql:///share_mail?sslmode=disable" $@
+    '';
+
+    db-drop.exec = ''
+      rm -r .devenv/state/postgres
+    '';
+  };
+
+  env.CGO_ENABLED = 0;
+
 }
